2e925c83328f5739b11cdeeb5007ff0150bcc3e2
Korzo <jatorolete@gmail.com>
2025-08-28 16:08:50 -0400

    se crea array y funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//crear un array para almacenar la lista 
+let ListaAmigos = [];
+
+//implementar una funcion para agregar amigos a la lista
+function AgregarAmigo() {
+    let nombre = document.getElementById("amigo").value.trim();
+
+    if (nombre !== "") {
+        ListaAmigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        actualizarLista();
+    }
+}
