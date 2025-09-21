0ffba24a0768300630f4de652f7fcc30d17ef63b
korzo <jatorolete@gmail.com>
2025-09-21 15:16:05 -0300

    actualizacion de challenge desde pc a github

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ function AgregarAmigo() {
     let nombre = document.getElementById("amigo").value.trim();
 
     if (nombre !== "") {
+        //validar que el nombre no esté en el array y el campo no esté vacío
+        if (ListaAmigos.includes(nombre)) {
+        alert("Ese nombre ya está en la lista.");//alerta nombre repetido
+        return;
+        }
+        //agregar el nuevo nombre al array y activar actualizar la lista en html
         ListaAmigos.push(nombre);
         document.getElementById("amigo").value = "";
         ActualizarLista();
+        console.log(ListaAmigos);
+        
+    } else {
+        alert("Por favor, ingresa un nombre válido.");//mostrar alerta campo invalido
     }
 }
 
-function ActualizarLista() {}
+function ActualizarLista() {
+    let listaHTML = document.getElementById("lista-amigos");
+    listaHTML.innerHTML = "";
+    for (let amigo of ListaAmigos) {
+        let nuevaLista = document.createElement("li");
+        nuevaLista.textContent = amigo;
+        listaHTML.appendChild(nuevaLista);
+    }
+}
+
